e52c51a39272d7b50577aa50483a94fe4eeeba9f
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-07 10:27:14 -0300

    feat: diretório de rotas

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-const { engine } = require('express-handlebars')
+const { engine } = require('express-handlebars');
+const router = require('./router/router')
 
-app.engine('handlebars', engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.send('Funcionando');
-});
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
