3370d4cf3d89bc295b6f7aab09539e37be412dcc
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-07 10:10:17 -0300

    feat: configurações do Express Handlebars

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const router = require('./router')
+const port = 3001;
+const { engine } = require('express-handlebars')
 
-app.use(express.json());
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.send('Funcionando');
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- src/router/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const filmesRouter = require('../router/filmesRouter')
-
-router.use('/filmes', filmesRouter);
-
-module.exports = router;
