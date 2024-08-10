95c1bf98a1455e5f6415c363b31ff106e0818149
Carlos Henrique <carlospaixao05@gmail.com>
2024-08-10 19:36:52 -0300

    feat: adicionar configuração do engine Handlebars para renderização de views

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const app = express();
 const port = 3001;
-const { engine } = require('express-handlebars');
+const {engine} = require('express-handlebars');
 const router = require('./router/router');
+const app = express();
 
-app.engine('handlebars', engine({ defaultLayout: 'main' }));
+// Configura o motor de templates Handlebars
+app.engine('handlebars', engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views/');
 
+// Usa o router definido no arquivo router/router.js
 app.use('/', router);
 
+// Inicia o servidor na porta 3001
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
