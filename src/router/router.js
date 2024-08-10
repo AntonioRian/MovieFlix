dd42ed2867ee977fdb670822d3bc972c9cd50337
Carlos Henrique <carlospaixao05@gmail.com>
2024-08-10 19:42:34 -0300

    refactor: organizar rotas com roteador separado para filmes e rota base para a página inicial

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const filmesRouter = require('./filmesRouter');
+const express = require('express'); // Importa o módulo Express
+const router = express.Router(); // Cria uma instância do roteador Express
+const filmesRouter = require('./filmesRouter'); // Importa o roteador de filmes
 
+// Define a rota base para renderizar a página inicial
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index'); // Renderiza a visão 'index'
 });
 
+// Encaminha as rotas que começam com '/filmes' para o roteador de filmes
 router.use('/filmes', filmesRouter);
 
-module.exports = router;
+module.exports = router; // Exporta o roteador para uso em outros arquivos
