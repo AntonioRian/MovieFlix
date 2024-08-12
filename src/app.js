131158be7a683730f66d12dd5b2fb4441f41ab0b
Vamberto Jr <juniorwan2012@gmail.com>
2024-08-12 10:45:05 -0300

    feat: configuração do arquivo .env e ajustes uso de variáveis ambiente.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const express = require('express');
-const port = 3001;
+const port = process.env.PORT || 3001;
 const {engine} = require('express-handlebars');
 const router = require('./router/router');
 const app = express();
