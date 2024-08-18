fe9ef3008ee105bae0a09dba63d13418e10aead4
AntonioRian <rian.crato@gmail.com>
2024-08-18 15:48:34 -0300

    feat: adicionar o local de armazenamento dos cards de filmes e renderizar na pagina inicial

---
+++ src/data/featuredMovies.js
@@ -0,0 +1,18 @@
+//filmes que vão ser renderizados na home page do site (featured movies)
+
+const featuredMovies = [
+    {imdbID: "tt6263850", Title: "Deadpool & Wolverine", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BNzRiMjg0MzUtNTQ1Mi00Y2Q5LWEwM2MtMzUwZDU5NmVjN2NkXkEyXkFqcGc@._V1_SX300.jpg" },
+    {imdbID: "tt4919268", Title: "Bad Boys: Ride or Die", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BY2U5YmQ3YjgtM2I2OC00YmM5LTkyM2MtN2I5Zjg2MDE0ODkwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg" },
+    {imdbID: "tt22022452", Title: "Inside Out 2", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BYTc1MDQ3NjAtOWEzMi00YzE1LWI2OWUtNjQ0OWJkMzI3MDhmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg" },
+    {imdbID: "tt26736843", Title: "The Watchers", Year: 2024, Poster:  "https://m.media-amazon.com/images/M/MV5BZjNlYjg5YzYtMmU4ZC00MzA4LWE5YWYtNTJlOTkyZmQ0YzdkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"},
+    {imdbID: "tt12037194", Title: "Furiosa: A Mad Max Saga", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BNWRlZTJhNGMtOWIyOS00YjRkLWExNjgtNDEwOTY1NGYxYmEwXkEyXkFqcGc@._V1_SX300.jpg"},
+    {imdbID: "tt17279496", Title: "Civil War", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BYTYyODhlODktYjUzNC00NjUyLWI1MzYtNmI0MTY3YTUxYjY2XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"},
+    {imdbID: "tt14539740", Title: "Godzilla x Kong: The New Empire", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BY2QwOGE2NGQtMWQwNi00M2IzLThlNWItYWMzNGQ5YWNiZDA4XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"},
+    {imdbID: "tt13433802", Title: "A Quiet Place: Day One", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BNGZmODU3ZDEtMjQwZC00NTA5LThmNWYtYzk5MmY5ZmM4NGIxXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"},
+    {imdbID: "tt21235248", Title: "Ghostbusters: Frozen Empire", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BNGE5MWJmZWYtN2ZlMi00ZjY1LTlhYTUtMzQ2Y2IxZWQyYTA2XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg"},
+    {imdbID: "tt23137390", Title: "Immaculate", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BNmYyZGQzM2YtYTY3My00NGE5LWIzMmQtMDIxMTFhMGIxZDhhXkEyXkFqcGdeQXVyMTY0Njc2MTUx._V1_SX300.jpg"},
+    {imdbID: "tt11389872", Title: "Kingdom of the Planet of the Apes", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BZGI4NTEwNTAtZDcwMi00MDkxLTg1OGYtNTZmMzE3ZDljNzVlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg"},
+    {imdbID: "tt27489557", Title: "Abigail", Year: 2024, Poster: "https://m.media-amazon.com/images/M/MV5BY2JlYjlkNzMtNTU3OS00NDVmLTk4NTYtYTM4ZTEwMGM4NDBiXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg"},
+];
+
+module.exports = featuredMovies;
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,11 @@
 const express = require('express'); // Importa o módulo Express
 const router = express.Router(); // Cria uma instância do roteador Express
 const filmesRouter = require('./filmesRouter'); // Importa o roteador de filmes
+const featuredMovies = require('../data/featuredMovies');
 
 // Define a rota base para renderizar a página inicial
 router.get('/', (req, res) => {
-    res.render('index'); // Renderiza a visão 'index'
+    res.render('index', { featuredMovies });
 });
 
 // Encaminha as rotas que começam com '/filmes' para o roteador de filmes
